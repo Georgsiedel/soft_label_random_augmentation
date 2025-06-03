a6ab0cbc4171f2ba920c375e5f6f65e3b1416e02
Georgsiedel <georgsiedel@gmx.de>
2025-06-03 10:02:15 +0200

    results

--- run_2.py
+++ run_2.py
@@ -2,23 +2,53 @@ from training import train
 
 if __name__ == "__main__":
 
-        for seed in [3,4]: 
+        for seed in [0,1,2,3,4]:      
+
                 train(seed = seed,
-                        dataset="TinyImageNet",
+                        dataset="CIFAR100",
                         random_cropping=0,
-                        trivial_augment=0,
+                        trivial_augment=1,
                         random_erasing=0,
-                        random_erasing_p=0.75,
+                        random_erasing_p=0.5,
                         random_erasing_max_scale=0.4,
-                        reweight=False
-                        )
+                        mapping_approach='label_smoothing')  
+                train(seed = seed,
+                        dataset="CIFAR10",
+                        random_cropping=0,
+                        trivial_augment=1,
+                        random_erasing=0,
+                        random_erasing_p=0.5,
+                        random_erasing_max_scale=0.4,
+                        mapping_approach='label_smoothing')  
                 train(seed = seed,
                         dataset="TinyImageNet",
                         random_cropping=0,
                         trivial_augment=1,
                         random_erasing=0,
-                        random_erasing_p=0.75,
+                        random_erasing_p=0.5,
                         random_erasing_max_scale=0.4,
-                        reweight=False
-                        )
-                
+                        mapping_approach='label_smoothing')  
+                train(seed = seed,
+                        dataset="CIFAR100",
+                        random_cropping=0,
+                        trivial_augment=0,
+                        random_erasing=1,
+                        random_erasing_p=0.5,
+                        random_erasing_max_scale=0.4,
+                        mapping_approach='label_smoothing')  
+                train(seed = seed,
+                        dataset="CIFAR10",
+                        random_cropping=0,
+                        trivial_augment=0,
+                        random_erasing=1,
+                        random_erasing_p=0.5,
+                        random_erasing_max_scale=0.4,
+                        mapping_approach='label_smoothing')  
+                train(seed = seed,
+                        dataset="TinyImageNet",
+                        random_cropping=0,
+                        trivial_augment=0,
+                        random_erasing=1,
+                        random_erasing_p=0.5,
+                        random_erasing_max_scale=0.4,
+                        mapping_approach='label_smoothing')  
