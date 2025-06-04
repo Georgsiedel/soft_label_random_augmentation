9ac8aa2a587842bdbdaada200ddda4c71c619c8b
Georg Siedel <georgsiedel@gmx.de>
2025-06-04 22:01:25 +0000

    results

--- run_2.py
+++ run_2.py
@@ -2,11 +2,12 @@ from training import train
 
 if __name__ == "__main__":
 
-        for seed in [0,1,2,3,4]:      
+
+        for seed in [1,2,3,4]:      
 
                 train(seed = seed,
                         dataset="CIFAR100",
-                        random_cropping=0,
+                        random_cropping=1,
                         trivial_augment=1,
                         random_erasing=0,
                         random_erasing_p=0.5,
@@ -14,7 +15,7 @@ if __name__ == "__main__":
                         mapping_approach='label_smoothing')  
                 train(seed = seed,
                         dataset="CIFAR10",
-                        random_cropping=0,
+                        random_cropping=1,
                         trivial_augment=1,
                         random_erasing=0,
                         random_erasing_p=0.5,
@@ -22,7 +23,7 @@ if __name__ == "__main__":
                         mapping_approach='label_smoothing')  
                 train(seed = seed,
                         dataset="TinyImageNet",
-                        random_cropping=0,
+                        random_cropping=1,
                         trivial_augment=1,
                         random_erasing=0,
                         random_erasing_p=0.5,
@@ -30,7 +31,7 @@ if __name__ == "__main__":
                         mapping_approach='label_smoothing')  
                 train(seed = seed,
                         dataset="CIFAR100",
-                        random_cropping=0,
+                        random_cropping=1,
                         trivial_augment=0,
                         random_erasing=1,
                         random_erasing_p=0.5,
@@ -38,7 +39,7 @@ if __name__ == "__main__":
                         mapping_approach='label_smoothing')  
                 train(seed = seed,
                         dataset="CIFAR10",
-                        random_cropping=0,
+                        random_cropping=1,
                         trivial_augment=0,
                         random_erasing=1,
                         random_erasing_p=0.5,
@@ -46,9 +47,56 @@ if __name__ == "__main__":
                         mapping_approach='label_smoothing')  
                 train(seed = seed,
                         dataset="TinyImageNet",
-                        random_cropping=0,
+                        random_cropping=1,
                         trivial_augment=0,
                         random_erasing=1,
                         random_erasing_p=0.5,
                         random_erasing_max_scale=0.4,
                         mapping_approach='label_smoothing')  
+
+        for seed in [3,4]:
+                train(seed = seed,
+                        dataset="TinyImageNet",
+                        random_cropping=0,
+                        trivial_augment=2,
+                        random_erasing=0,
+                        random_erasing_p=0.75,
+                        random_erasing_max_scale=0.4,
+                        reweight=False
+                        )
+                train(seed = seed,
+                        dataset="TinyImageNet",
+                        random_cropping=0,
+                        trivial_augment=2,
+                        random_erasing=0,
+                        random_erasing_p=0.75,
+                        random_erasing_max_scale=0.4,
+                        reweight=True
+                        )
+                train(seed = seed,
+                        dataset="TinyImageNet",
+                        random_cropping=1,
+                        trivial_augment=0,
+                        random_erasing=0,
+                        random_erasing_p=0.75,
+                        random_erasing_max_scale=0.4,
+                        reweight=False
+                        )
+                train(seed = seed,
+                        dataset="TinyImageNet",
+                        random_cropping=2,
+                        trivial_augment=0,
+                        random_erasing=0,
+                        random_erasing_p=0.75,
+                        random_erasing_max_scale=0.4,
+                        reweight=False
+                        )
+                train(seed = seed,
+                        dataset="TinyImageNet",
+                        random_cropping=2,
+                        trivial_augment=0,
+                        random_erasing=0,
+                        random_erasing_p=0.75,
+                        random_erasing_max_scale=0.4,
+                        reweight=True
+                        )
--- run_3.py
+++ run_3.py
@@ -2,45 +2,20 @@ from training import train
 
 if __name__ == "__main__":
 
+                
         for seed in [0,1,2,3,4]:
+                
                 train(seed = seed,
-                        dataset="CIFAR100",
+                        dataset="TinyImageNet",
                         random_cropping=0,
                         trivial_augment=0,
                         random_erasing=1,
-                        random_erasing_p=0.5,
-                        random_erasing_max_scale=0.2,
-                        reweight=True
-                        )
-                train(seed = seed,
-                        dataset="CIFAR100",
-                        random_cropping=0,
-                        trivial_augment=0,
-                        random_erasing=1,
-                        random_erasing_p=0.75,
-                        random_erasing_max_scale=0.2,
-                        reweight=True
-                        )
-                train(seed = seed,
-                        dataset="CIFAR100",
-                        random_cropping=0,
-                        trivial_augment=0,
-                        random_erasing=1,
-                        random_erasing_p=1.0,
-                        random_erasing_max_scale=0.2,
-                        reweight=True
-                        )
-                train(seed = seed,
-                        dataset="CIFAR100",
-                        random_cropping=0,
-                        trivial_augment=0,
-                        random_erasing=2,
-                        random_erasing_p=0.5,
-                        random_erasing_max_scale=0.2,
+                        random_erasing_p=0.25,
+                        random_erasing_max_scale=0.8,
                         reweight=False
                         )
                 train(seed = seed,
-                        dataset="CIFAR100",
+                        dataset="TinyImageNet",
                         random_cropping=0,
                         trivial_augment=0,
                         random_erasing=2,
@@ -49,11 +24,39 @@ if __name__ == "__main__":
                         reweight=False
                         )
                 train(seed = seed,
-                        dataset="CIFAR100",
+                        dataset="TinyImageNet",
                         random_cropping=0,
                         trivial_augment=0,
                         random_erasing=2,
-                        random_erasing_p=1.0,
+                        random_erasing_p=0.75,
+                        random_erasing_max_scale=0.2,
+                        reweight=True
+                        )
+                train(seed = seed,
+                        dataset="TinyImageNet",
+                        random_cropping=0,
+                        trivial_augment=1,
+                        random_erasing=1,
+                        random_erasing_p=0.25,
+                        random_erasing_max_scale=0.8,
+                        reweight=False
+                        )
+                train(seed = seed,
+                        dataset="TinyImageNet",
+                        random_cropping=0,
+                        trivial_augment=1,
+                        random_erasing=2,
+                        random_erasing_p=0.75,
                         random_erasing_max_scale=0.2,
                         reweight=False
                         )
+                train(seed = seed,
+                        dataset="TinyImageNet",
+                        random_cropping=0,
+                        trivial_augment=1,
+                        random_erasing=2,
+                        random_erasing_p=0.75,
+                        random_erasing_max_scale=0.2,
+                        reweight=True
+                        )
+               
