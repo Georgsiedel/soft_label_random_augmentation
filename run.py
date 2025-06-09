3cebef74e7d900d77ea184affde74f0e061496f3
Georg Siedel <georgsiedel@gmx.de>
2025-06-09 21:46:54 +0000

    res

--- run.py
+++ run.py
@@ -1,36 +1,6 @@
 from training import train
 
 if __name__ == "__main__":
-
-        for seed in [1,2,3]: 
-                
-                train(seed = seed,
-                        dataset="CIFAR10",
-                        random_cropping=0,
-                        trivial_augment=0,
-                        random_erasing=1,
-                        random_erasing_p=0.25,
-                        random_erasing_max_scale=0.8,
-                        reweight=True
-                        )
-                train(seed = seed,
-                        dataset="CIFAR10",
-                        random_cropping=0,
-                        trivial_augment=0,
-                        random_erasing=2,
-                        random_erasing_p=0.75,
-                        random_erasing_max_scale=0.2,
-                        reweight=False
-                        )
-                train(seed = seed,
-                        dataset="CIFAR10",
-                        random_cropping=0,
-                        trivial_augment=0,
-                        random_erasing=2,
-                        random_erasing_p=0.75,
-                        random_erasing_max_scale=0.2,
-                        reweight=True
-                        )
         
         for seed in [3,4]:
                 
