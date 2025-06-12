0191d0eaaf90381bf7edd4888c0bcf9d67e0e7ff
Georg Siedel <georgsiedel@gmx.de>
2025-06-12 10:10:07 +0000

    results

--- run_2.py
+++ run_2.py
@@ -1,24 +1,8 @@
 from training import train
 
 if __name__ == "__main__":
-
-        for seed in [4]:
-                                
-                train(seed = seed,
-                        dataset="CIFAR100",
-                        random_cropping=0,
-                        trivial_augment=1,
-                        random_erasing=0,
-                        patch_gaussian=3
-                        )
         
         for seed in [0]:
-                train(seed = seed,
-                        dataset="TinyImageNet",
-                        random_cropping=0,
-                        trivial_augment=2,
-                        random_erasing=0,
-                        )
                 train(seed = seed,
                         dataset="TinyImageNet",
                         random_cropping=0,
