f1dc9afb662f9d5b630231328a970cfc05285239
Georg Siedel <georgsiedel@gmx.de>
2025-07-18 20:54:59 +0000

    results resnext

--- run_3.py
+++ run_3.py
@@ -2,7 +2,7 @@ from training import train
 
 if __name__ == "__main__":
         
-        for seed in [0,1,2,3,4]:        
+        for seed in [1,2,3,4]:        
                 train(seed = seed,
                         dataset="CIFAR10",
                         random_cropping=1,
